7a0bbef770a394e96babc2e16e3afbae2e240e1c
coreone <teixeira@broadinstitute.org>
2015-03-28 16:21:57 -0400

    Add dockutil and some dock items

--- modules/people/manifests/coreone.pp
+++ modules/people/manifests/coreone.pp
@@ -1,10 +1,35 @@
 class people::coreone {
     include cord
 #    include cyberduck
+    include dockutil
     include dropbox
     include fitbit
     include googledrive
     include hipchat
     include sequel_pro
     include spotify
+
+    dockutil::item { 'Add HipChat':
+        item     => "/Applications/HipChat.app",
+        label    => "HipChat",
+        action   => "add"
+    }
+
+    dockutil::item { 'Add Firefox':
+        item     => "/Applications/Firefox.app",
+        label    => "Firefox",
+        action   => "add"
+    }
+
+    dockutil::item { 'Add Chrome':
+        item     => "/Applications/Google Chrome.app",
+        label    => "Chrome",
+        action   => "add"
+    }
+
+    dockutil::item { 'Add CoRD':
+        item     => "/Applications/CoRD.app",
+        label    => "CoRD",
+        action   => "add"
+    }
 }
