55d8ea09fd51493779fcee260a2f5661fde9b1f0
coreone <teixeira@broadinstitute.org>
2015-05-15 19:55:32 -0400

    Add chrome, osx, sublime_text, and brewcask, reorganized the Dock, added some homebrew packages, and configured some OSX items

--- modules/people/manifests/coreone.pp
+++ modules/people/manifests/coreone.pp
@@ -1,4 +1,6 @@
 class people::coreone {
+    include brewcask
+    include chrome
     include cord
 #    include cyberduck
     include dockutil
@@ -13,7 +15,12 @@ class people::coreone {
     include robomongo
     include sequel_pro
     include spotify
+    include sublime_text::v2
     include wget
+
+    # sublime_text::v2::package { 'Emmet':
+    #     source => 'sergeche/emmet-sublime'
+    # }
 
     dockutil::item { 'Remove Mail':
         item     => "/Applications/Mail.app",
@@ -75,67 +82,100 @@ class people::coreone {
         action   => "remove"
     }
 
+    dockutil::item { 'Remove Microsoft Document Connection':
+        item     => "/Applications/Microsoft Office 2011/Microsoft Document Connection.app",
+        label    => "Microsoft Document Connection",
+        action   => "remove"
+    }
+
+    dockutil::item { 'Remove Remote Desktop Connection':
+        item     => "/Applications/Remote Desktop Connection.app",
+        label    => "Remote Desktop Connection",
+        action   => "remove"
+    }
+
+    dockutil::item { 'Add Launchpad':
+        item     => "/Applications/Launchpad.app",
+        label    => "Launchpad",
+        action   => "add",
+        position => 1
+    }
+
+    dockutil::item { 'Add Mission Control':
+        item     => "/Applications/Mission Control.app",
+        label    => "Mission Control",
+        action   => "add",
+        position => 2
+    }
+
+    dockutil::item { 'Add App Store':
+        item     => "/Applications/App Store.app",
+        label    => "App Store",
+        action   => "add",
+        position => 3
+    }
+
     dockutil::item { 'Add Safari':
         item     => "/Applications/Safari.app",
         label    => "Safari",
         action   => "add",
-        position => 3
+        position => 4
     }
 
     dockutil::item { 'Add Firefox':
         item     => "/Applications/Firefox.app",
         label    => "Firefox",
         action   => "add",
-        position => 4
+        position => 5
     }
 
     dockutil::item { 'Add Chrome':
         item     => "/Applications/Google Chrome.app",
         label    => "Google Chrome",
         action   => "add",
-        position => 5
+        position => 6
     }
 
     dockutil::item { 'Add HipChat':
         item     => "/Applications/HipChat.app",
         label    => "HipChat",
         action   => "add",
-        position => 6
+        position => 7
     }
 
     dockutil::item { 'Add Terminal':
         item     => "/Applications/Utilities/Terminal.app",
         label    => "Terminal",
         action   => "add",
-        position => 7
+        position => 8
     }
 
     dockutil::item { 'Add Sublime Text':
         item     => "/Applications/Sublime Text 2.app",
         label    => "Sublime Text 2",
         action   => "add",
-        position => 8
+        position => 9
     }
 
     dockutil::item { 'Add CoRD':
         item     => "/Applications/CoRD.app",
         label    => "CoRD",
         action   => "add",
-        position => 9
+        position => 10
     }
 
     dockutil::item { 'Add Sequel Pro':
         item     => "/Applications/Sequel Pro.app",
         label    => "Sequel Pro",
         action   => "add",
-        position => 10
+        position => 11
     }
 
     dockutil::item { 'Add Spotify':
         item     => "/Applications/Spotify.app",
         label    => "Spotify",
         action   => "add",
-        position => 11
+        position => 12
     }
 
     ruby_gem { 'activesupport for all rubies':
@@ -186,4 +226,35 @@ class people::coreone {
 #         ruby_version => '*'
 #     }
 
+    # Homebrew: Apps.....
+    package {[
+        'cowsay',
+        'docker',
+        'docker-machine',
+        'figlet'
+    ]:
+    }
+
+    # Homebrew: Cask (GUI) Apps.....
+    package {[
+#        'tinkertool',
+        'seashore',
+    ]:
+        provider => 'brewcask',
+    }
+
+    include osx::global::enable_keyboard_control_access
+    include osx::finder::unhide_library
+    include osx::global::expand_save_dialog
+
+    osx::dock::hot_corner { 'Bottom Right':
+        action => 'Start Screen Saver'
+    }
+    osx::dock::hot_corner { 'Bottom Left':
+        action => 'Disable Screen Saver'
+    }
+
+    class {'osx::global::natural_mouse_scrolling':
+        enabled => false
+    }
 }
